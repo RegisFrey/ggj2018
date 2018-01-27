3c662917ffdf0c411f225369821bddf51e22dc9a
Regis Frey <RegisFrey@Darkfirestudios.com>
2018-01-27 10:56:12 -0800

    Backing color to grid letter

--- Assets/GridLetter.cs
+++ Assets/GridLetter.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class GridLetter : MonoBehaviour {
 
 	public Text letter;
+	public Image backing;
 
 	public void SetLetter (System.Char l) {
 		letter.text = l.ToString();
@@ -13,5 +14,9 @@ public class GridLetter : MonoBehaviour {
 	
 	public void LetterColor(Color c) {
 		letter.color = c;
+	}
+	
+	public void BackingColor(Color c) {
+		backing.color = c;
 	}
 }
