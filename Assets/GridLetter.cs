0bc5f1ea1f792de716b6332214d83a8ce80ed363
Regis Frey <RegisFrey@Darkfirestudios.com>
2018-01-27 14:54:59 -0800

    Letters can be glitched

--- Assets/GridLetter.cs
+++ Assets/GridLetter.cs
@@ -7,14 +7,24 @@ public class GridLetter : MonoBehaviour {
 
 	public Text letter;
 	public Image backing;
+	public Image glitch;
 
 	public void SetLetter (System.Char l) {
 		letter.text = l.ToString();
 	}
 	
-	public void LetterStyle(Style s) {
+	public void Unglitch () {
+		glitch.gameObject.SetActive(false);
+	}
+	
+	public void Glitch () {
+		glitch.gameObject.SetActive(true);
+	}
+	
+	public void LetterStyle (Style s) {
 		letter.color = s.fgColor;
-		backing.gameObject.SetActive(true); // could disable if style matches main bkg style
+		// backing.gameObject.SetActive(true); // could disable if style matches main bkg style
 		backing.color = s.bkgColor;
+		glitch.color = s.fgColor;
 	}
 }
