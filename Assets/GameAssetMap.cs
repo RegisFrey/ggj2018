b563cf94c9607fca66f00facf3603d7a4d8a2d20
Regis Frey <RegisFrey@Darkfirestudios.com>
2018-01-27 14:58:16 -0800

    AssetMap for looking up images

---
+++ Assets/GameAssetMap.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AssetMap", menuName = "New AssetMap", order = 0)]
+public class GameAssetMap : ScriptableObject {
+	public List<Sprite> glitches;
+}
