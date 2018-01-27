686124fd671119cdf0dd487290c56552bfeef0ca
Regis Frey <RegisFrey@Darkfirestudios.com>
2018-01-27 09:45:09 -0800

    Grid of Letter

---
+++ Assets/GridLetter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridLetter : MonoBehaviour {
+
+	public Text letter;
+
+	public void SetLetter (System.Char l) {
+		letter.text = l.ToString();
+	}
+	
+	public void LetterColor(Color c) {
+		letter.color = c;
+	}
+}
---
+++ Assets/LetterGrid.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Serialization;
+
+public class LetterGrid : MonoBehaviour {
+	
+	public GridLetter gridLetterPrefab;
+	public int gridWidth = 32;
+	public int gridHeight = 32;
+	
+	public int gridCols = 12;
+	public int gridRows = 6;
+	public List<GridLetter> grid;
+	
+	public Color targetColor;
+	public Color defaultColor;
+	
+	[Header("Character Sets")]
+	public string correctedAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+	//public String corruptedAlpha = "αß¢Đëƒ*ʜ!յ<|3n0ρ@Ի$7μѵM%{}"; 
+	public string corruptedSymbol = "!@#$%^&*()?1234567890~.,<>{}[]\\|/"; 
+	
+	[Header("Word Lists")]
+	[FormerlySerializedAs("codewords")]
+	public List<string> codeWords;
+	public List<string> noiseWords;
+	
+	[Header("Runtime")]
+	[Range(0, 1)]
+	public float percentageCorruption;
+	public int offset;
+	public string targetWord;
+	public string plaintext;
+	public string ciphertext;
+	
+
+	// Use this for initialization
+	void Start () {
+		CreatePlainText();
+		// Setup grid
+		grid = new List<GridLetter>();
+    for(int i = 0; i < GridCharacters(); i++)
+    {
+        grid.Add( 
+					CreateGridLetter( 
+						new Vector2(
+							((i % gridCols) * gridWidth), 
+							(Mathf.Floor(i / gridCols) * gridHeight)
+						) 
+					)
+				);
+    }
+		StartCoroutine("UpdateLetters", 1f);
+	}
+	
+	IEnumerator UpdateLetters(float numSecs)
+	{
+		while(true){
+			for(int i = 0; i < grid.Count; i++)
+			{
+				// wrap i + offset
+				RenderGridLetter(grid[i], plaintext[i + offset], percentageCorruption, false);
+			}
+			yield return new WaitForSeconds(numSecs);
+		}	
+	}
+	
+	int GridCharacters() {
+		return gridCols * gridRows;
+	}
+	
+	void CreatePlainText() {
+		int gridCharacters = GridCharacters();
+		while(plaintext.Length < gridCharacters) {
+			int randIndex = Random.Range(0, noiseWords.Count);
+			plaintext = plaintext + ' ' + noiseWords[randIndex];
+		}
+		// corrupt plain text a bit
+	}
+	
+	GridLetter CreateGridLetter(Vector2 position) {
+		GridLetter gl = Instantiate(gridLetterPrefab, new Vector3(position.x, position.y, 0), Quaternion.identity) as GridLetter;
+		gl.gameObject.transform.SetParent(gameObject.transform, false);
+		return gl;
+	}
+
+	
+	// corruption 0-1, 
+	void RenderGridLetter(GridLetter gl, System.Char letter, float corruption, bool target) {
+		// UN-corruption % chance to get colorized
+		if(target && Random.value > (1 - corruption) ) {
+			gl.LetterColor(targetColor);
+		}
+		// 3% chance a non-target gets colorized
+		else if(Random.value > 0.97) {
+			gl.LetterColor(targetColor);
+		}
+		else
+		{
+			gl.LetterColor(defaultColor);
+		}
+		// corruption % chance will randomly replace with symbol
+		if(Random.value > corruption) {
+			gl.SetLetter(letter);
+		}
+		else
+		{
+			gl.SetLetter(
+				corruptedSymbol[Random.Range(0, corruptedSymbol.Length)]
+			);
+		}
+	}
+}
