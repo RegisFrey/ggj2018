8ce119ab3f2dfc30f2276210560e75bb9055ff20
dilarasemerci <dilarasem@gmail.com>
2018-01-27 15:28:53 -0800

    Game manager dont destroy

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -27,6 +27,7 @@ public class GameManager : MonoBehaviour {
         {
             _instance = this;
         }
+        DontDestroyOnLoad(gameObject);
     }
 
     public void SetPercentageCorruptions(float first, float second)
