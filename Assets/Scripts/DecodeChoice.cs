14d032610a63520d3bbe663962ed9a397ac98ea3
Regis Frey <RegisFrey@Darkfirestudios.com>
2018-01-28 08:06:46 -0800

    Level data format (should be able to setup level data assets in Unity, will work on loading them)

---
+++ Assets/Scripts/DecodeChoice.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Choice", menuName = "Decode Choice", order = 0)]
+public class DecodeChoice : ScriptableObject {
+	public string name;
+	public Sprite symbol;
+	public string notes;
+}
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Level", menuName = "Level", order = 0)]
+public class Level : ScriptableObject {
+	
+	public string name;
+	[Space(10)]
+	[TextArea]
+	public string notes;
+	[Space(10)]
+	public float seconds;
+	public StyleSet style;
+	
+	[System.Serializable]
+	public class Dialogue {
+		public string text;
+		public string speaker;
+		// audio asset voice line
+	}
+	[Space(10)]
+	public List<Dialogue> introDialogue;
+	[Space(10)]
+	public List<string> cluewotds;
+	[Header("Either define fake words to pick from or write plaintext")]
+	public List<string> fakewotds;
+	public string plaintext; 
+	
+	[Space(10)]
+	public List<DecodeChoice> choices;
+	public int answer;
+	
+}
