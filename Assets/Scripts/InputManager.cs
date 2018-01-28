862b857e2b1d41062224b52a058fe29f586b4be9
dilarasemerci <dilarasem@gmail.com>
2018-01-27 18:11:22 -0800

    Input manager

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using XInputDotNetPure; // Required in C#
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+    bool playerIndexSet = false;
+    PlayerIndex playerIndex;
+    GamePadState state;
+    GamePadState prevState;
+
+
+    private static InputManager _instance;
+    public static InputManager Instance
+    {
+        get { return _instance; }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        // Find a PlayerIndex, for a single player game
+        // Will find the first controller that is connected ans use it
+        if (!playerIndexSet || !prevState.IsConnected)
+        {
+            for (int i = 0; i < 4; ++i)
+            {
+                PlayerIndex testPlayerIndex = (PlayerIndex)i;
+                GamePadState testState = GamePad.GetState(testPlayerIndex);
+                if (testState.IsConnected)
+                {
+                    Debug.Log(string.Format("GamePad found {0}", testPlayerIndex));
+                    playerIndex = testPlayerIndex;
+                    playerIndexSet = true;
+                }
+            }
+        }
+
+        prevState = state;
+        state = GamePad.GetState(playerIndex);
+    }
+
+    public void VibrateController(float vibrationLeft, float vibratioRight)
+    {
+        GamePad.SetVibration(playerIndex, vibrationLeft, vibratioRight);
+    }
+
+    public PlayerIndex PlayerIndex
+    {
+        get { return playerIndex; }
+    }
+    public GamePadTriggers Triggers
+    {
+        get { return state.Triggers; }
+    }
+    public float LeftTrigger
+    {
+        get { return state.Triggers.Left; }
+    }
+    public float RightTrigger
+    {
+        get { return state.Triggers.Right; }
+    }
+    
+}
--- Assets/Scripts/TriggerInput.cs
+++ Assets/Scripts/TriggerInput.cs
@@ -9,11 +9,6 @@ public class TriggerInput : MonoBehaviour {
 
     public Text statusLabel;
 
-    bool playerIndexSet = false;
-    PlayerIndex playerIndex;
-    GamePadState state;
-    GamePadState prevState;
-
     float targetLeft;
     float targetRight;
 
@@ -27,9 +22,9 @@ public class TriggerInput : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        float vibrationLeft = (state.Triggers.Left < targetLeft) ? (1f/targetLeft) * state.Triggers.Left : (state.Triggers.Left-1f)/(targetLeft-1f);
-        float vibratioRight = (state.Triggers.Right < targetRight) ? (1f / targetRight) * state.Triggers.Right : (state.Triggers.Right - 1f) / (targetRight - 1f);
-        GamePad.SetVibration(playerIndex, vibrationLeft, vibratioRight);
+        float vibrationLeft = (InputManager.Instance.LeftTrigger < targetLeft) ? (1f/targetLeft) * InputManager.Instance.LeftTrigger : (InputManager.Instance.LeftTrigger - 1f)/(targetLeft-1f);
+        float vibratioRight = (InputManager.Instance.RightTrigger < targetRight) ? (1f / targetRight) * InputManager.Instance.RightTrigger : (InputManager.Instance.RightTrigger - 1f) / (targetRight - 1f);
+        InputManager.Instance.VibrateController(vibrationLeft, vibratioRight);
 
         if (statusLabel != null) {
             if (vibrationLeft > correctnessThreshold && vibratioRight > correctnessThreshold)
@@ -43,29 +38,9 @@ public class TriggerInput : MonoBehaviour {
         }
 
         GameManager.Instance.SetPercentageCorruptions(1f - vibrationLeft, 1f - vibratioRight);
-        SoundManager.Instance.SetPitch(1+(state.Triggers.Left-targetLeft)*1.5f);
-    }
+        SoundManager.Instance.SetPitch(1+(InputManager.Instance.LeftTrigger - targetLeft)*1.5f);
 
-    private void Update()
-    {
-        // Find a PlayerIndex, for a single player game
-        // Will find the first controller that is connected ans use it
-        if (!playerIndexSet || !prevState.IsConnected)
-        {
-            for (int i = 0; i < 4; ++i)
-            {
-                PlayerIndex testPlayerIndex = (PlayerIndex)i;
-                GamePadState testState = GamePad.GetState(testPlayerIndex);
-                if (testState.IsConnected)
-                {
-                    Debug.Log(string.Format("GamePad found {0}", testPlayerIndex));
-                    playerIndex = testPlayerIndex;
-                    playerIndexSet = true;
-                }
-            }
-        }
+        Debug.Log("Vib: "+ vibrationLeft + " " + vibratioRight);
 
-        prevState = state;
-        state = GamePad.GetState(playerIndex);
-    }
+    }   
 }
